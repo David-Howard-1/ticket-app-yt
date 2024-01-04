d99cb7310533b480d652607caed53598c3e829e0
David H <davidweh7878@hotmail.com>
2024-01-04 15:08:10 -0600

    first commit

---
+++ app/ticket/[id]/page.jsx
@@ -0,0 +1,8 @@
+
+const Ticket = ({ params }) => {
+  return (
+    <div>Ticket {params.id}</div>
+  )
+}
+
+export default Ticket
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,17 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        nav: '#EEEDE8',
+        page: '#F4F5F6',
+        tomato: '#F06543',
+        card: '#313638',
+        'card-hover': '#1C2021',
+        'brown-accent': '#E17714',
+        'brown-accent-hover': '#A9590F',
+        'default-text': '#131516',
+      },
     },
   },
   plugins: [],
-}
+};
